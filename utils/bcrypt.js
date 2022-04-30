dc1af2e2234857e0c31cf3ee246bd6cab4de3b83
manish-nagarro-1101 <84827518+manish-nagarro-1101@users.noreply.github.com>
2022-04-30 22:51:07 +0530

    updated

--- utils/bcrypt.js
+++ utils/bcrypt.js
@@ -1,5 +1,5 @@
 var bcrypt = require('bcryptjs');
 var salt = bcrypt.genSaltSync(10);
 var hash = bcrypt.hashSync("hello", salt);
-console.log(salt);
+console.log('jklfjdlasjkjds',salt);
 console.log(hash);
