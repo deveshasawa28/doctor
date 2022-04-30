58f879e83ab1080c55bfb8281d636d8d36a53e72
devesh <devesh@gmail.com>
2022-04-30 23:54:29 +0530

    product

--- public/javascripts/products.js
+++ public/javascripts/products.js
@@ -10,27 +10,16 @@ https://en.wikipedia.org/wiki/Map_(higher-order_function)
 */
 function success() {
   console.log('success');
-  let data = JSON.parse(xhttp.response);
-
-  let rows = data.map((row) =>
-    <tr key={JSON.stringify(row)}>
-      <td> {row.id}</td>
-      <td> {row.username}</td>
-      <td> {row.description}</td>
-      <td> {row.due}</td>
-    </tr>
-  );
-  console.log('products loaded from backend....')
-  console.log(rows);
-  const productsList = rows.map((item, index) =>
+    let data = JSON.parse(xhttp.response);
+  
+  const productsList = data.map((item, index) =>
     <div className="row" key={index}>
       <div className="col-sm-6 col-md-4">
-        <div className="thumbnail">
-          <img src="..." alt="..." />
+        <div className="thumbnail"> 
             <div className="caption">
-              <h3>{item.product_name}</h3>
-              <p>{item.product_des}</p>
-              <p>{item.ammount}</p>
+              <h3>product name:       {item.product_name}</h3>
+              <p>product description: {item.product_des}</p>
+              <p>amount:               {item.ammount}</p>
               <p><a href="#" className="btn btn-primary" role="button">Add to cart</a> <a href="#" className="btn btn-default" role="button">Remove from cart</a></p>
             </div>
         </div>
